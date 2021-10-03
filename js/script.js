abf915fda2109c7e8003bf6fd1ac9db7576fe1a3
amirestes <amirestes@gmail.com>
2021-10-03 18:05:12 -0500

    added async function to pull repo

---
+++ js/script.js
@@ -0,0 +1,26 @@
+//global variable to select the div with a class of overview. this div is where your profile information will appear.
+const profileElement = document.querySelector(".overview");
+const username = "amirestes";
+
+const githubProfile = async function () {
+    const response = await fetch(
+        `https://api.github.com/users/${username}`
+    );
+    const data = await response.json();
+    userInfo(data);
+};
+
+//function to display fetched user information
+
+const userInfo = function (data) {
+    const div = document.createElement("div");
+    div.classList.add("user-info");
+    div.innerHTML = 
+    `<p><strong>Name:</strong> ${data.name} </p>
+    <p><strong>Bio:</strong> ${data.bio}</p>
+    <p><strong>Number of public repos:</strong> ${data.public_repos} </p>`;
+
+    profileElement.append(div);
+};
+
+githubProfile();
