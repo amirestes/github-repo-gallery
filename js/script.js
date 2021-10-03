fd6798a835cee73a828682b1e6d1cb1fec6c73c3
amirestes <amirestes@gmail.com>
2021-10-03 18:10:18 -0500

    corrected div html

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const githubProfile = async function () {
     );
     const data = await response.json();
     userInfo(data);
+    
 };
 
 //function to display fetched user information
@@ -16,10 +17,15 @@ const userInfo = function (data) {
     const div = document.createElement("div");
     div.classList.add("user-info");
     div.innerHTML = 
-    `<p><strong>Name:</strong> ${data.name} </p>
-    <p><strong>Bio:</strong> ${data.bio}</p>
-    <p><strong>Number of public repos:</strong> ${data.public_repos} </p>`;
-
+    `<figure>
+        <img alt="user avatar" src=${data.avatar_url}/>
+    </figure>
+    <div>
+        <p><strong>Name:</strong> ${data.name} </p>
+        <p><strong>Bio:</strong> ${data.bio} </p>
+        <p><strong>Number of public repos:</strong> ${data.public_repos} </p>
+    </div>`;
+    
     profileElement.append(div);
 };
 
